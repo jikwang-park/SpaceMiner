39a804edc4d15842bf9436085423eafb7aa55732
황규영 <kyuy9214@gmail.com>
2025-03-21 15:36:04 +0900

    feat: 유닛 전략 패턴 인터페이스 추가
    Attack 구조체
    IAttackable 인터페이스

---
+++ Unity/Assets/Scripts/Units/Interface/Attack.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Attack
+{
+    public int damage; // ToDo: 이후 큰 수 체계 잡히면 그 때 적용할 것
+    public bool critical;
+}

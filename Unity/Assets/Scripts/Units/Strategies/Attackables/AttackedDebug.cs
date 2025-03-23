bccd6d4f5734d2e14530147bd973c19f1c4d29b4
황규영 <kyuy9214@gmail.com>
2025-03-23 16:05:23 +0900

    feat: 전략 패턴 추가
    피격 시 디버그 메시지 출력

---
+++ Unity/Assets/Scripts/Units/Strategies/Attackables/AttackedDebug.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackedDebug : MonoBehaviour, IAttackable
+{
+    public void OnAttack(GameObject attacker, Attack attack)
+    {
+        if (attack.critical)
+        {
+            Debug.Log("크리티컬!");
+        }
+        Debug.Log($"{attacker.name} => {gameObject.name} : {attack.damage}");
+    }
+}

3935d3faf5119153813f617d86fc935a5852c23d
황규영 <kyuy9214@gmail.com>
2025-03-23 18:44:57 +0900

    feat: 배경 생성 구현
    유닛 매니저 미구현으로 인한 스테이지 생성, 삭제 테스트 구현

--- Unity/Assets/Scripts/Stage/Stage.cs
+++ Unity/Assets/Scripts/Stage/Stage.cs
@@ -1,8 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class Stage : MonoBehaviour
 {
+    private int unitCount = 0;
+    private bool isNextStageSpawned = false;
 
+    [SerializeField]
+    private AssetReferenceGameObject stage;
+
+    [SerializeField]
+    private Transform enterPosition;
+
+    [SerializeField]
+    private Transform exitPosition;
+
+    [SerializeField]
+    private Transform stageNextPosition;
+
+    [SerializeField]
+    private Transform unit; // TODO: 유닛 파티 매니저로부터 받아올 수 있게 변경할 예정 - 250323 HKY
+
+    private void Update()
+    {
+        if (!isNextStageSpawned && unit.position.z > enterPosition.position.z)
+        {
+            isNextStageSpawned = true;
+
+            Addressables.InstantiateAsync(stage, stageNextPosition.position, stageNextPosition.rotation).Completed+= (handle)=> handle.Result.GetComponent<Stage>().unit = unit;
+        }
+
+        if (unit.position.z > exitPosition.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
 }

ca46a011ca2e79e251dcda8135d4858d4e2f1c0d
황규영 <kyuy9214@gmail.com>
2025-03-25 10:33:44 +0900

    chore: 스크립트 이동 및 클래스명 수정
    오브젝트 풀 매니저 매니저 폴더로 이동
    러시 셀렉트 버튼 클래스명 버튼에서 토글로 변경

--- Unity/Assets/Scripts/Ui/Stage/RushSelectToggle.cs
+++ Unity/Assets/Scripts/Ui/Stage/RushSelectToggle.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RushSelectButton : MonoBehaviour
+public class RushSelectToggle : MonoBehaviour
 {
     private const string Ascend = "Ascend";
     private const string Repeat = "Repeat";
 
     [SerializeField]
     private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        if (Variables.stageMode == StageMode.Repeat)
+        {
+            GetComponent<Toggle>().isOn = false;
+        }
+    }
 
     public void OnRushToggleChanged(bool isOn)
     {

298c3699878b3ca18be3fe4b27250eb8e4348783
jikwang-park <pdk00137@gmail.com>
2025-03-28 15:20:01 +0900

    chore 머지전 커밋

---
+++ Unity/Assets/Scripts/Units/Manager/UnitUiManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UnitUiManager 
+{
+    
+
+
+
+    public void SetUnitName(TextMeshProUGUI text,UnitTypes type)
+    {
+        
+    }
+}

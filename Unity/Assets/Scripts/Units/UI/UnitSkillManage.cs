3a0fe9d1b22fa122c49f6a283297e8408b19eaf0
jikwang-park <pdk00137@gmail.com>
2025-04-02 14:01:58 +0900

    feat 스킬 강화Ui 추가

--- Unity/Assets/Scripts/Units/UI/UnitSkillManage.cs
+++ Unity/Assets/Scripts/Units/UI/UnitSkillManage.cs
@@ -5,19 +5,5 @@ using UnityEngine.UI;
 
 public class UnitSkillManage : MonoBehaviour
 {
-    [SerializeField]
-    private Button closeButton;
 
-
-
-
-    private void Start()
-    {
-        closeButton.onClick.AddListener(() => OnClickCloseButton());
-    }
-
-    private void OnClickCloseButton()
-    {
-        gameObject.SetActive(false);
-    }
 }

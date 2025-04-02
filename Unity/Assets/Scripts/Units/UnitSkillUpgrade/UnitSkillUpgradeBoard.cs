148925b2b8b55acff88c80e5447599f89d4afb12
jikwang-park <pdk00137@gmail.com>
2025-04-02 15:55:01 +0900

    fix 프리팹 수정

---
+++ Unity/Assets/Scripts/Units/UnitSkillUpgrade/UnitSkillUpgradeBoard.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSkillUpgradeBoard : MonoBehaviour
+{
+   
+
+
+
+    public void ShowFirstOpened()
+    {
+        // 처음 스킬 업그레이드 버튼 클릭했을시에 탱커 -> 노말 기준으로 뜨게 여기서 설정해야됌
+
+    }
+}

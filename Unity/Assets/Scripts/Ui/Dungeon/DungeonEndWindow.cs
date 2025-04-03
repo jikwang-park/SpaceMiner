d7c92a427e53592ec2569981699fe559b419f862
황규영 <kyuy9214@gmail.com>
2025-04-03 21:45:50 +0900

    feat: 던전 재입장 버튼 조건 구현
    조건에 통과하면 던전 재입장 불가능하도록 구현

--- Unity/Assets/Scripts/Ui/Dungeon/DungeonEndWindow.cs
+++ Unity/Assets/Scripts/Ui/Dungeon/DungeonEndWindow.cs
@@ -4,15 +4,20 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DungeonEndWindow : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI nextText;
+    [SerializeField]
+    private TextMeshProUGUI messageText;
 
     private float closeTime;
     private WaitForSeconds wait = new WaitForSeconds(1f);
 
+    [SerializeField]
+    private Button nextButton;
     private bool isCleared;
 
     public void Set(bool isCleared)
@@ -24,15 +29,31 @@ public class DungeonEndWindow : MonoBehaviour
     {
         if (isCleared)
         {
-            if(Variables.currentDungeonStage == DataTableManager.DungeonTable.CountOfStage(Variables.currentDungeonType))
+            messageText.text = "Cleared";
+            bool lastStageCondition = Variables.currentDungeonStage == DataTableManager.DungeonTable.CountOfStage(Variables.currentDungeonType);
+            var curStage = DataTableManager.DungeonTable.GetData(Variables.currentDungeonType, Variables.currentDungeonStage);
+            bool keyCondition = ItemManager.GetItemAmount(curStage.DungeonKeyID) >= curStage.KeyCount;
+
+            if (lastStageCondition)
             {
+                nextButton.interactable = ItemManager.GetItemAmount(curStage.DungeonKeyID) >= curStage.KeyCount;
+                
                 nextText.text = "Retry";
-                return;
             }
-            nextText.text = "Next";
+            else
+            {
+                nextText.text = "Next";
+                var nextStage = DataTableManager.DungeonTable.GetData(Variables.currentDungeonType, Variables.currentDungeonStage + 1);
+
+                bool powerCondition = Variables.powerLevel > nextStage.ConditionPower;
+                bool planetCondition = SaveLoadManager.Data.stageSaveData.highPlanet > nextStage.ConditionPlanet;
+                keyCondition = ItemManager.GetItemAmount(nextStage.DungeonKeyID) >= nextStage.KeyCount;
+                nextButton.interactable = powerCondition && planetCondition && keyCondition;
+            }
         }
         else
         {
+            messageText.text = "Failed";
             nextText.text = "Retry";
         }
     }

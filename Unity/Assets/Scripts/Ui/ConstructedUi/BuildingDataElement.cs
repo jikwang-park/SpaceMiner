97643ebd56b23c3e1a3c9bfd36aa268c13f4e78e
jikwang-park <pdk00137@gmail.com>
2025-04-09 11:28:37 +0900

    fix 맥스레벨시 버튼 비활성화 추가

--- Unity/Assets/Scripts/Ui/ConstructedUi/BuildingDataElement.cs
+++ Unity/Assets/Scripts/Ui/ConstructedUi/BuildingDataElement.cs
@@ -16,6 +16,8 @@ public class BuildingDataElement: MonoBehaviour
     private TextMeshProUGUI levelText;
     [SerializeField]
     private TextMeshProUGUI constructionExplanText;
+    [SerializeField]
+    private Image lockedImage;
 
 
     [SerializeField]
@@ -53,7 +55,6 @@ public class BuildingDataElement: MonoBehaviour
         this.data = data;
         level = 0;
         SetLevelData(level);
-        isLocked = true;
     }
 
     public void SetLevelData(int level)
@@ -65,6 +66,15 @@ public class BuildingDataElement: MonoBehaviour
         maxLevel = data[level].MaxLevel;
         needItemCount = data[level].NeedCount;
         currentType = data[level].Type;
+        if(level == 0)
+        {
+            isLocked = true;
+        }
+        else
+        {
+            isLocked = false;
+        }
+        SetFirstUpgrade(isLocked);
         SetConstructionInfo();
     }
 
@@ -76,16 +86,16 @@ public class BuildingDataElement: MonoBehaviour
     private void SetConstructionInfo()
     {
         SetLevelText(level);
-        SetConstructionExplanText();
+        SetConstructionExplanText(currentType);
     }
 
     private void SetLevelText(int level)
     {
         levelText.text = $"LV.{level}";
     }
-    private void SetConstructionExplanText()
+    private void SetConstructionExplanText(BuildingTable.BuildingType type)
     {
-        constructionExplanText.text = $"{currentType.ToString()} \n {itemId} 가 {needItemCount} 개 필요합니다";
+        constructionExplanText.text = $"{type.ToString()} \n {itemId} 가 {needItemCount} 개 필요합니다";
     }
     public void SetData(BuildingTable.BuildingType type,int level)
     {
@@ -101,6 +111,9 @@ public class BuildingDataElement: MonoBehaviour
 
     public void LevelUp()
     {
+        if (level == maxLevel)
+            return;
+
         level++;
         SetLevelData(level);
 
@@ -110,14 +123,32 @@ public class BuildingDataElement: MonoBehaviour
         SaveLoadManager.Data.buildingData.buildingLevels[currentType] = level;
         SaveLoadManager.SaveGame();
     }
-    private bool CheckMaxLevel(int level)
+    private bool IsMaxLevel(int level)
     {
-        if(level > maxLevel)
+        if(level == maxLevel)
         {
             upgradeButtonText.text = "최대레벨달성";
             return true;
         }
         return false;
+    }
+
+    private void Update()
+    {
+        if (IsMaxLevel(level))
+        {
+            upgradeButton.interactable = false;
+        }
+    }
+
+    private void SetFirstUpgrade(bool isLocked)
+    {
+        if(isLocked)
+        {
+            lockedImage.gameObject.SetActive(true);
+        }
+
+        lockedImage.gameObject.SetActive(false);
     }
     private void OnClickUpgradeButton()
     {
@@ -125,10 +156,7 @@ public class BuildingDataElement: MonoBehaviour
         {
             isLocked = false;
         }
-        if(CheckMaxLevel(level))
-        {
-            upgradeButton.interactable = false;
-        }
+       
         LevelUp();
 
     }

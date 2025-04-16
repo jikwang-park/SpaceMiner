7e5a9d01ada46b39cbbd1fd98ae2d6ae1454dcd1
설현기 <shgi7694@gmail.com>
2025-04-16 11:27:55 +0900

    fix: UICanvas에 병사 관리, 채굴 인벤토리 UI 수정

--- Unity/Assets/Scripts/Ui/UnitManage/Gacha/GachaDescribeUI.cs
+++ Unity/Assets/Scripts/Ui/UnitManage/Gacha/GachaDescribeUI.cs
@@ -14,7 +14,7 @@ public class GachaDescribeUI : MonoBehaviour
     }
     public void Initialize(GachaTable.Data data)
     {
-
         describeText.text = DataTableManager.StringTable.GetData(data.DetailStringID); //250331 HKY 데이터형 변경
+
     }
 }

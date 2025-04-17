14235144c921fcfdacb83678b2622327cb8793e5
jikwang-park <pdk00137@gmail.com>
2025-04-18 00:27:55 +0900

    feat 플레이어 Ui 피통 추가

--- Unity/Assets/Scripts/Units/Manager/UnitUiManager.cs
+++ Unity/Assets/Scripts/Units/Manager/UnitUiManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,50 @@ using UnityEngine.UI;
 
 public class UnitUiManager : MonoBehaviour
 {
-    private Dictionary<UnitTypes,Slider> hpDic = new Dictionary<UnitTypes,Slider>();
+    private Dictionary<UnitTypes, Slider> hpDic = new Dictionary<UnitTypes, Slider>();
 
     [SerializeField]
     private StageManager stageManager;
+    private UnitPartyManager unitPartyManager;
 
+    [SerializeField]
+    private Slider tankerSlider;
+    [SerializeField]
+    private Slider dealerSlider;
+    [SerializeField]
+    private Slider healerSlider;
 
 
     private void Awake()
     {
         stageManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<StageManager>();
-        
     }
 
     private void Start()
     {
+        stageManager.UnitPartyManager.OnUnitCreated += DoUnitCreated;
     }
 
+    public void DoUnitCreated()
+    {
+        foreach (UnitTypes type in Enum.GetValues(typeof(UnitTypes)))
+        {
+            var unit = stageManager.UnitPartyManager.GetUnit(type);
+            switch (type)
+            {
+                case UnitTypes.Tanker:
+                    unit.GetComponent<UnitStats>().onHpChanged += (amount) => { tankerSlider.value = amount; };
+                    break;
+                case UnitTypes.Dealer:
+                    unit.GetComponent<UnitStats>().onHpChanged += (amount) => { dealerSlider.value = amount; };
 
-   
+                    break;
+                case UnitTypes.Healer:
+                    unit.GetComponent<UnitStats>().onHpChanged += (amount) => { healerSlider.value = amount; };
+
+                    break;
+            }
+        }
+    }
+
 }

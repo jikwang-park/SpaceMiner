932332fd4ef173a17ab69bce4f927f12b691bb34
황규영 <kyuy9214@gmail.com>
2025-04-20 20:31:08 +0900

    chore: 던전 리스트 입장 버튼에 현재 던전 키 갯수 보이도록 수정

--- Unity/Assets/Scripts/Ui/Dungeon/DungeonEnterRow.cs
+++ Unity/Assets/Scripts/Ui/Dungeon/DungeonEnterRow.cs
@@ -11,19 +11,38 @@ public class DungeonEnterRow : MonoBehaviour
     private LocalizationText detailText;
     [SerializeField]
     private AddressableImage iconImage;
+    [SerializeField]
+    private LocalizationText keyAmountText;
 
     private int dungeonType;
 
     public event System.Action<int> OnButtonClicked;
 
+    private int keyItemID;
+    private BigNumber needItemCount;
+
     public void SetType(int type)
     {
         dungeonType = type;
         var dungeonData = DataTableManager.DungeonTable.GetData(type, 1);
-
+        keyItemID = dungeonData.NeedKeyItemID;
+        needItemCount = dungeonData.NeedKeyItemCount;
         nameText.SetString(dungeonData.NameStringID);
         detailText.SetString(dungeonData.NameStringID);
         iconImage.SetSprite(dungeonData.SpriteID);
+        keyAmountText.SetStringArguments(needItemCount.ToString(), ItemManager.GetItemAmount(dungeonData.NeedKeyItemID).ToString());
+
+        ItemManager.OnItemAmountChanged += OnItemAmountChanged;
+    }
+
+    private void OnItemAmountChanged(int itemId, BigNumber amount)
+    {
+        if (keyItemID != itemId)
+        {
+            return;
+        }
+        keyAmountText.SetStringArguments(needItemCount.ToString(), amount.ToString());
+
     }
 
     public void OnButtonClick()

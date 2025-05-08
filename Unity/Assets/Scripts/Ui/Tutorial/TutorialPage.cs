7c463b34fe0aea6fce470b702a97c30e2fc644bd
황규영 <kyuy9214@gmail.com>
2025-05-08 11:42:35 +0900

    feat: 튜토리얼 재확인 창 구현

---
+++ Unity/Assets/Scripts/Ui/Tutorial/TutorialPage.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPage : MonoBehaviour
+{
+    [SerializeField]
+    private LocalizationText titleText;
+
+    [SerializeField]
+    private LocalizationText detailText;
+
+    [SerializeField]
+    private AddressableImage image;
+
+    [SerializeField]
+    private TextMeshProUGUI indexText;
+
+    private int index;
+
+    [SerializeField]
+    private Button closeButton;
+
+    private List<TutorialTable.Data> datas;
+
+    private TutorialTable.QuestTypes tutorialType;
+
+    private void Start()
+    {
+        if (closeButton is null)
+        {
+            Show(TutorialTable.QuestTypes.FirstPlay);
+        }
+    }
+
+    public void Show(TutorialTable.QuestTypes type)
+    {
+        tutorialType = type;
+        datas = DataTableManager.TutorialTable.GetDatas(tutorialType);
+        index = 0;
+        ShowData();
+
+        if (closeButton is not null)
+        {
+            closeButton.interactable = SaveLoadManager.Data.TutorialOpened[tutorialType];
+        }
+    }
+
+    public void IndexChange(bool isUp)
+    {
+        if (isUp && index + 1 < datas.Count)
+        {
+            ++index;
+            ShowData();
+            if (!SaveLoadManager.Data.TutorialRewardGot[tutorialType]
+                && index + 1 == datas.Count && datas[index].RewardItemID != 0)
+            {
+                SaveLoadManager.Data.TutorialRewardGot[tutorialType] = true;
+                SaveLoadManager.Data.TutorialOpened[tutorialType] = true;
+
+                if (closeButton is not null)
+                {
+                    closeButton.interactable = true;
+                }
+
+                ItemManager.AddItem(datas[index].RewardItemID, datas[index].RewardItemCount);
+
+                SaveLoadManager.SaveGame();
+            }
+        }
+        else if (!isUp && index > 0)
+        {
+            --index;
+            ShowData();
+        }
+    }
+
+    private void ShowData()
+    {
+        titleText.SetString(datas[index].NameStringID);
+        image.SetSprite(datas[index].SpriteID);
+        indexText.text = (index + 1).ToString();
+        detailText.SetString(datas[index].DetailStringID);
+    }
+}

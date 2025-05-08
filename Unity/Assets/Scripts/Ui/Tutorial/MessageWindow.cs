4cbed000d7a237445c4cf44d7def96f94af01e31
황규영 <kyuy9214@gmail.com>
2025-05-08 15:59:32 +0900

    feat: 컨텐츠 해금 기능 구현

---
+++ Unity/Assets/Scripts/Ui/Tutorial/MessageWindow.cs
@@ -0,0 +1,45 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageWindow : MonoBehaviour
+{
+    [SerializeField]
+    private Image background;
+
+    [SerializeField]
+    private float showTime = 1.5f;
+
+    [SerializeField]
+    private LocalizationText text;
+
+    private float endTime;
+
+    private void Update()
+    {
+        float currentTime = Time.time;
+
+        if (currentTime > endTime)
+        {
+            gameObject.SetActive(false);
+        }
+
+        var ratio = (endTime - currentTime) / showTime;
+
+        Color tempColor = background.color;
+        tempColor.a = ratio;
+        background.color = tempColor;
+    }
+
+    public void Show(int planet, int stage)
+    {
+        gameObject.SetActive(true);
+        text.SetStringArguments(planet.ToString(), stage.ToString());
+        Color tempColor = background.color;
+        tempColor.a = 1f;
+        background.color = tempColor;
+        endTime = Time.time + showTime;
+    }
+}

8c8497fec79777b18b40ed7a10cacfdd67e63f23
jikwang-park <pdk00137@gmail.com>
2025-05-14 11:04:32 +0900

    fix 스킬강화 활성화 비활성화 버튼 이미지 수정

--- Unity/Assets/Scripts/Units/Manager/UnitUiManager.cs
+++ Unity/Assets/Scripts/Units/Manager/UnitUiManager.cs
@@ -6,53 +6,12 @@ using UnityEngine.UI;
 
 public class UnitUiManager : MonoBehaviour
 {
-    //private Dictionary<UnitTypes, Slider> hpDic = new Dictionary<UnitTypes, Slider>();
-
-    //[SerializeField]
-    //private StageManager stageManager;
-    //private UnitPartyManager unitPartyManager;
-
     [SerializeField]
     private Slider tankerSlider;
     [SerializeField]
     private Slider dealerSlider;
     [SerializeField]
     private Slider healerSlider;
-
-
-    //private void Awake()
-    //{
-    //    stageManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<StageManager>();
-    //}
-
-    //private void Start()
-    //{stageManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<StageManager>();
-    //}
-
-    //public void DoUnitCreated()
-    //{
-    //    foreach (UnitTypes type in Enum.GetValues(typeof(UnitTypes)))
-    //    {
-    //        var unit = stageManager.UnitPartyManager.GetUnit(type);
-    //        var stats = unit.GetComponent<UnitStats>();
-    //        switch (type)
-    //        {
-    //            case UnitTypes.Tanker:
-    //                stats.OnHpChanged += (amount) => { tankerSlider.value = amount; };
-
-    //                break;
-    //            case UnitTypes.Dealer:
-    //                stats.OnHpChanged += (amount) => { dealerSlider.value = amount; };
-
-    //                break;
-    //            case UnitTypes.Healer:
-    //                stats.OnHpChanged += (amount) => { healerSlider.value = amount; };
-
-    //                break;
-    //        }
-    //    }
-    //}
-
     public void SetUnitHpBar(UnitTypes type, UnitStats stats)
     {
         switch (type)

7ef88bd9d07c46114e6e42e40d58120a393a18fd
황규영 <kyuy9214@gmail.com>
2025-05-16 14:06:03 +0900

    fix: 저장소 전투 클리어 시 무조건 클리어한 스테이지 기준으로 나오는 버그 수정

--- Unity/Assets/Scripts/Stage/StageFSM/MineStageStatusMachine.cs
+++ Unity/Assets/Scripts/Stage/StageFSM/MineStageStatusMachine.cs
@@ -372,8 +372,11 @@ public class MineStageStatusMachine : StageStatusMachine
             SaveLoadManager.Data.mineBattleData.lastClearTime = TimeManager.Instance.GetEstimatedServerTime();
 
             var datas = DataTableManager.MiningBattleTable.GetDatas(battleData.PlanetTableID);
-            SaveLoadManager.Data.stageSaveData.ClearedMineStage[battleData.PlanetTableID] = Mathf.Min(battleData.Stage + 1, datas[datas.Count - 1].Stage);
-            SaveLoadManager.Data.stageSaveData.HighMineStage[battleData.PlanetTableID] = battleData.Stage;
+            if (SaveLoadManager.Data.stageSaveData.ClearedMineStage[battleData.PlanetTableID] < battleData.Stage)
+            {
+                SaveLoadManager.Data.stageSaveData.ClearedMineStage[battleData.PlanetTableID] = battleData.Stage;
+                SaveLoadManager.Data.stageSaveData.HighMineStage[battleData.PlanetTableID] = Mathf.Min(battleData.Stage + 1, datas[datas.Count - 1].Stage);
+            }
 
             SaveLoadManager.SaveGame();
 
